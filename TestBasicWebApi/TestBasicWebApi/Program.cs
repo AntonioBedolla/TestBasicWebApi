f197f8a1de2b020031210bab5d2e1d0e0e062dc8
ANTONIOBEDO9E0C\antoniobedolla <fred-mercury@hotmail.com>
2025-03-29 18:37:54 -0600

    Se implemento que  en swager se pudiera poner el token para probar los demas apis(Program.cs) Se cambio la key de JWT porque era muy corta y mandaba error(appsetting.json)

--- TestBasicWebApi/TestBasicWebApi/Program.cs
+++ TestBasicWebApi/TestBasicWebApi/Program.cs
@@ -1,13 +1,16 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 using TestBasicWebApi.Data;
+using TestBasicWebApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 //Configurar Db context
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<JwtService>();
 
 // Configurar JWT
 var key = Encoding.ASCII.GetBytes(builder.Configuration["Jwt:Key"]);
@@ -35,7 +38,41 @@ builder.Services.AddAuthentication(options =>
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// Agregar servicios de Swagger
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "TestBasicApi",
+        Description = "Este back es un proyecto personal de practica para aprender WebApi .Net Core",
+         Version = "v1"
+    });
+
+    //Configurar autenticación con JWT en Swagger
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "Ingrese el token JWT en el formato 'Bearer {token}'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] { }
+        }
+    });
+});
 
 var app = builder.Build();
 
